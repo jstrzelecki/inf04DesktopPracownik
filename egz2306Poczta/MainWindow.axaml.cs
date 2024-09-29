a4e3064e1b82b8f5183210353e55fc3a167e924f
jarek_lnx <j@proton.com>
2024-09-29 21:44:18 +0200

    Implement  OnCheckPriceButton method

--- egz2306Poczta/MainWindow.axaml.cs
+++ egz2306Poczta/MainWindow.axaml.cs
@@ -1,17 +1,21 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 
 namespace egz2306Poczta;
 
 public partial class MainWindow : Window
 {
-    private List<RadioButton> _radioButtons;
+    private readonly RadioButton[] _radiobuttonsGroup;
     public MainWindow()
     {
         InitializeComponent();
-        _radioButtons =
+        _radiobuttonsGroup =
         [
             PostcardRadioButton,
             LetterRadioButton,
@@ -21,6 +25,21 @@ public partial class MainWindow : Window
 
     private void OnCheckPriceButton(object? sender, RoutedEventArgs e)
     {
-        var selectedOption = _radioButtons.FirstOrDefault( rb => rb.IsChecked == true );
+        var selectedRadioButton = _radiobuttonsGroup.FirstOrDefault(rb=>rb.IsChecked == true);
+        if (selectedRadioButton != null)
+        {
+            (string imagePath, string priceInfo) = selectedRadioButton.Tag switch
+            {
+                "Postcard" => ("avares://egz2306Poczta/Assets/postcard.png", "Cena 1 zł"),
+                "Letter" => ("avares://egz2306Poczta/Assets/letter.png", "Cena 1,5 zł"),
+                "Package" => ("avares://egz2306Poczta/Assets/postcard.png", "Cena 10 zł"),
+                _ => ("Assets/letter.png", "Cena: 1,5 zł")
+            };
+
+            PriceTextBlock.Text = priceInfo;
+            using var stream = AssetLoader.Open(new Uri(imagePath));
+            ParcelImage.Source = new Bitmap(stream);
+        }
+        
     }
 }
