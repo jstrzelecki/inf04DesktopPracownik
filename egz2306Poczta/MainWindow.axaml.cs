64b08748257696c86680cd46ee928b6784343c8b
jarek_lnx <j@proton.com>
2024-09-29 20:27:53 +0200

    Define Style Classes for TextBlock Header

--- egz2306Poczta/MainWindow.axaml.cs
+++ egz2306Poczta/MainWindow.axaml.cs
@@ -1,11 +1,26 @@
+using System.Collections.Generic;
+using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace egz2306Poczta;
 
 public partial class MainWindow : Window
 {
+    private List<RadioButton> _radioButtons;
     public MainWindow()
     {
         InitializeComponent();
+        _radioButtons =
+        [
+            PostcardRadioButton,
+            LetterRadioButton,
+            PackageRadioButton
+        ];
+    }
+
+    private void OnCheckPriceButton(object? sender, RoutedEventArgs e)
+    {
+        var selectedOption = _radioButtons.FirstOrDefault( rb => rb.IsChecked == true );
     }
 }
